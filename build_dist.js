5f3d51b3cace3834987bfcd69c980c9d322b085e
Bryan Housel <bryan@mapbox.com>
2020-03-09 23:24:46 -0400

    Update build_dist to generate legacy imagery.xml

--- build_dist.js
+++ build_dist.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 const LocationConflation = require('@ideditor/location-conflation');
 const prettyStringify = require('json-stringify-pretty-compact');
 const shell = require('shelljs');
+const xmlbuilder2 = require('xmlbuilder2');
 
-const featureCollection = require('./dist/featureCollection.json');
 const sources = require('./dist/sources.json').sources;
+const featureCollection = require('./dist/featureCollection.json');
 const loco = new LocationConflation(featureCollection);
+
 
 buildAll();
 
@@ -37,9 +39,10 @@ function buildAll() {
   fs.writeFileSync('dist/featureCollection.min.json', JSON.stringify(featureCollection) );
   fs.writeFileSync('dist/sources.min.json', JSON.stringify({ sources: sources }) );
 
-  generateCombined(sources);
-  generateLegacyImageryGeojson(sources);
-  generateLegacyImageryJson(sources);
+  generateCombined();
+  generateLegacyImageryGeojson();
+  generateLegacyImageryJson();
+  generateLegacyImageryXml();
 
   console.timeEnd(END);
   console.log('');
@@ -57,6 +60,7 @@ function iconPath(icon) {
     return `https://cdn.jsdelivr.net/gh/ideditor/imagery-index@master/dist/images/${icon}`;
   }
 }
+
 function dateString() {
   const d = new Date();
   return JSON.stringify(d)
@@ -102,7 +106,7 @@ function dateString() {
 //   ]
 // }
 //
-function generateCombined(sources) {
+function generateCombined() {
   process.stdout.write('📦  dist/combined.geojson  ');
 
   let keepFeatures = {};
@@ -157,7 +161,7 @@ function generateCombined(sources) {
 //   ]
 // }
 //
-function generateLegacyImageryGeojson(sources) {
+function generateLegacyImageryGeojson() {
   process.stdout.write('📦  dist/legacy_imagery.geojson  ');
 
   let keepFeatures = [];
@@ -222,7 +226,7 @@ function generateLegacyImageryGeojson(sources) {
 //   ]
 // }
 //
-function generateLegacyImageryJson(sources) {
+function generateLegacyImageryJson() {
   process.stdout.write('📦  dist/legacy_imagery.json  ');
 
   let keepSources = [];
@@ -277,6 +281,113 @@ function generateLegacyImageryJson(sources) {
 
   fs.writeFileSync('dist/legacy_imagery.json', prettyStringify(keepSources) );
   fs.writeFileSync('dist/legacy_imagery.min.json', JSON.stringify(keepSources) );
+
+  process.stdout.write(colors.green('✓\n'));
+}
+
+
+// `generateLegacyImageryXml`
+// Generate an editor-layer-index style `imagery.xml`
+// Like `imagery.geojson` but XML for JOSM.
+// https://josm.openstreetmap.de/wiki/Maps#Documentation
+//
+// <?xml version='1.0' encoding='utf-8'?>
+// <imagery>
+//   <entry>
+//     <id>Berlin-2011</id>
+//     <name>Berlin aerial photography 2011</name>
+//     …
+//     <shape>
+//       <point lon="13.2953" lat="52.392"/>
+//       <point lon="13.295" lat="52.4008"/>
+//       …
+//     </shape>
+//   </entry>
+//     …
+// </imagery>
+//
+function generateLegacyImageryXml() {
+  process.stdout.write('📦  dist/legacy_imagery.xml  ');
+
+  let root = xmlbuilder2.create({ version: '1.0', encoding: 'UTF-8' });
+  let imagery = root.ele('imagery');
+
+  Object.values(sources).forEach(source => {
+    let entry = imagery.ele('entry');
+
+    if (source.overlay)      { entry.att('overlay', true);  }
+    if (source.best)         { entry.att('eli-best', true); }
+
+    if (source.name)         { entry.ele('name').txt(source.name); }
+    if (source.id)           { entry.ele('id').txt(source.id); }
+    if (source.category)     { entry.ele('category').txt(source.category); }
+    if (source.type)         { entry.ele('type').txt(source.type); }
+    if (source.description)  { entry.ele('description').txt(source.description); }
+    if (source.url)          { entry.ele('url').txt(source.url); }
+    if (source.max_zoom)     { entry.ele('max-zoom').txt(source.max_zoom); }
+    if (source.min_zoom)     { entry.ele('min-zoom').txt(source.min_zoom); }
+    if (source.license_url)  { entry.ele('permission-ref').txt(source.license_url); }
+    if (source.icon)         { entry.ele('icon').txt(iconPath(source.icon)); }
+
+    // todo - country_code? default?
+
+    if (source.start_date) {
+      let date = entry.ele('date');
+      if (source.end_date && source.end_date === source.start_date) {
+        date.txt(source.start_date);
+      } else {
+        date.txt(source.start_date + ';' + (source.end_date || '-'));
+      }
+    }
+
+    if (source.attribution) {
+      if (source.attribution.url)  { entry.ele('attribution-url').txt(source.attribution.url); }
+      if (source.attribution.text) { entry.ele('attribution-text').txt(source.attribution.text); }
+    }
+
+    // gather projections
+    if (source.available_projections) {
+      let projections = entry.ele('projections');
+      source.available_projections.forEach(proj => projections.ele('code').txt(proj));
+    }
+
+    // gather bounds and polygon shapes
+    const feature = loco.resolveLocationSet(source.locationSet);
+    if (feature.geometry && feature.id !== 'Q2') {   // Q2 = whole world
+      let bounds = entry.ele('bounds');
+
+      let rings = [];  // gather shapes (outer rings)
+      if (feature.geometry.type === 'Polygon') {
+        rings = feature.geometry.coordinates.slice();  // copy whole polygon
+      } else if (feature.geometry.type === 'MultiPolygon') {
+        rings = [];
+        feature.geometry.coordinates.forEach(ring => {
+          rings.push(ring[0].slice());    // copy each outer ring
+        });
+      }
+
+      let minLat = -90, maxLat = 90, minLon = -180, maxLon= 180;
+      rings.forEach(ring => {
+        let shape = bounds.ele('shape');
+        ring.forEach(point => {
+          shape.ele('point').att('lat', point[1]).att('lon', point[0]);
+          if (minLon < point[0])  { minLon = point[0]; }
+          if (maxLon > point[0])  { maxLon = point[0]; }
+          if (minLat < point[1])  { minLat = point[1]; }
+          if (maxLat > point[1])  { maxLat = point[1]; }
+        })
+      });
+
+      bounds
+        .att('min-lat', minLat)
+        .att('min-lon', minLon)
+        .att('max-lat', maxLat)
+        .att('max-lon', maxLon);
+    }
+  });
+
+  fs.writeFileSync('dist/legacy_imagery.xml', root.end({ prettyPrint: true }) );
+  fs.writeFileSync('dist/legacy_imagery.min.xml', root.end() );
 
   process.stdout.write(colors.green('✓\n'));
 }
